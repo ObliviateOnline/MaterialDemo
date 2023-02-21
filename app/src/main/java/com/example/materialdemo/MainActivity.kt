f0d104efc27007c0fbf6934df070a20b53f88b77
rufeiqq <36234958+ObliviateOnline@users.noreply.github.com>
2023-02-21 23:27:54 +0800

    滑动菜单
    first commit 组件学习

--- app/src/main/java/com/example/materialdemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdemo/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.materialdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.materialdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+        binding.navView.setCheckedItem(R.id.navCall)
+        binding.navView.setNavigationItemSelectedListener {
+            binding.drawerLayout.closeDrawers()
+            true
+        }
     }
 }
