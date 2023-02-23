29a1f0a68f424ad51db6e74071a278c1db952b7a
rufeiqq <36234958+ObliviateOnline@users.noreply.github.com>
2023-02-23 22:58:41 +0800

    change toolbar and drawerLayout
    change toolbar and drawerLayout

--- app/src/main/java/com/example/materialdemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdemo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.materialdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.example.materialdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +22,28 @@ class MainActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.setHomeAsUpIndicator(R.drawable.ic_menu)
         }
+
+
+
         binding.navView.setCheckedItem(R.id.navCall)
         binding.navView.setNavigationItemSelectedListener {
             binding.drawerLayout.closeDrawers()
             true
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
+            R.id.backup -> Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
